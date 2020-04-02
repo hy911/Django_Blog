c8bb3b1667d20ff5b6a4fd2cc170685ae31ef1a7
hy911 <690322863@qq.com>
2020-04-02 16:32:14 +0800

    基础Ｍｏｄｅｌ创建

---
+++ Blog/blog/models.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    '''
+    分类
+    '''
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    '''
+    标签
+    '''
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    '''
+    文章
+    '''
+    #标题
+    title = models.TextField()
+
+    #文章创建时间和最后修改时间
+    create_time = models.DateField()
+    modified_time = models.DateField()
+
+    #文章摘要
+    excerpt = models.CharField(max_length=200,blank=True)
+
+    #类型关联,标题关联
+    category =models.ForeignKey(Category,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag,blank=True)
+
+    #作者
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
