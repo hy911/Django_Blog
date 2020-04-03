25c9c44b68fbc140bc7da6366fc18c7755c50d06
hy911 <690322863@qq.com>
2020-04-03 16:31:57 +0800

    更改admin汉化并开始增加detail视图

--- Blog/blog/models.py
+++ Blog/blog/models.py
@@ -2,11 +2,20 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+from django.utils import timezone
+
+
 class Category(models.Model):
     '''
     分类
     '''
     name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
 
 class Tag(models.Model):
     '''
@@ -14,23 +23,42 @@ class Tag(models.Model):
     '''
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+
 class Post(models.Model):
     '''
     文章
     '''
-    #标题
-    title = models.TextField()
+    # 标题
+    title = models.TextField('标题', max_length=70)
 
-    body = models.TextField()
-    #文章创建时间和最后修改时间
-    create_time = models.DateField()
-    modified_time = models.DateField()
+    body = models.TextField('正文')
+    # 文章创建时间和最后修改时间
+    create_time = models.DateField('创建时间', default=timezone.now)
+    modified_time = models.DateField('修改时间')
 
-    #文章摘要
-    excerpt = models.CharField(max_length=200,blank=True)
+    # 文章摘要
+    excerpt = models.CharField('摘要', max_length=200, blank=True)
 
-    #类型关联,标题关联
-    category =models.ForeignKey(Category,on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag,blank=True)
-    #作者
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    # 类型关联,标题关联
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True)
+    # 作者
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
--- Blog/blog/views.py
+++ Blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from blog.models import Post
@@ -7,4 +7,9 @@ from blog.models import Post
 
 def index(request):
     post_list = Post.objects.all().order_by('-create_time')
-    return render(request,'index.html',context={'post_list':post_list})
+    return render(request, 'blog/index.html', context={'post_list':post_list})
+
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/detail.html',context={'post':post})
